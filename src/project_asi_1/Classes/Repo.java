c38b60aeabfce697c6838b6fa79710f05475b735
Lucas <lucasroux72@gmail.com>
2019-02-26 11:31:43 +0100

    Repo class

---
+++ src/project_asi_1/Classes/Repo.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project_asi_1.Classes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Lucas
+ */
+@Entity
+@Table(name = "repo", schema = "OVAJ")
+public class Repo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_repo")
+    private int id;
+    @Column(name = "nom")
+    private String nom;
+    @Column(name = "path")
+    private String path;
+    @OneToOne
+    @JoinColumn(name = "repo_id_groupe", referencedColumnName = "id_groupe")
+    private Groupe groupe;
+
+    public Repo(int id, String nom, String path, Groupe groupe) {
+        this.id = id;
+        this.nom = nom;
+        this.path = path;
+        this.groupe = groupe;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public Groupe getGroupe() {
+        return groupe;
+    }
+
+    public void setGroupe(Groupe groupe) {
+        this.groupe = groupe;
+    }
+
+}
