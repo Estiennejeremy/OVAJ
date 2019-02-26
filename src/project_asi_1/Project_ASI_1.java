243ef2b0d4acfcbfe337222b526a6ab3c448b900
Dashin <estienne.jeremy70@gmail.com>
2019-02-26 11:49:22 +0100

    revert

--- src/project_asi_1/Project_ASI_1.java
+++ src/project_asi_1/Project_ASI_1.java
@@ -1,49 +1,4 @@
-<<<<<<< Updated upstream
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package project_asi_1;
 
-import java.io.IOException;
-import java.sql.SQLException;
-import project_asi_1.Classes.DAO.EleveDAO;
-import project_asi_1.Classes.Eleve;
-import project_asi_1.Classes.Groupe;
-
-/**
- *
- * @author ESTIENNE
- */
-public class Project_ASI_1 {
-
-    /**
-     * @param args the command line arguments
-     * @throws java.io.IOException
-     * @throws java.sql.SQLException
-     */
-    public static void main(String[] args) throws IOException, SQLException {
-
-        // TODO code application logic here
-//        JFrame myFrame = new JFrame("frame");
-//        myFrame.setSize(1080, 720);
-//        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        myFrame.add(new project_asi_1.Views.accueil());
-//        myFrame.setVisible(true);
-        Eleve e = new Eleve("test", "test2", "test3", "test4");
-        Groupe g = new Groupe("yes");
-        EleveDAO d = new EleveDAO();
-        d.saveEleve(e);
-
-    }
-}
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package project_asi_1;
 
 import java.io.IOException;
@@ -80,4 +35,4 @@ public class Project_ASI_1 {
 
     }
 }
->>>>>>> Stashed changes
+
