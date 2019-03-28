769bb12d3a1669bd6484c8164962a107b8004eaf
Dashin <estienne.jeremy70@gmail.com>
2019-03-28 08:19:46 +0100

    Update eleves_creer.java

--- src/project_asi_1/Views/eleves_creer.java
+++ src/project_asi_1/Views/eleves_creer.java
@@ -209,12 +209,13 @@ public class eleves_creer extends javax.swing.JPanel {
         } else {
             try {
                 EleveDAO eleveDao = new EleveDAO();
+                 Eleve eleve = new Eleve(jTextFieldEleve_nom.getText(), jTextFieldEleve_prenom.getText(), jTextFieldEleve_mail.getText());
                 eleveDao.saveEleve(eleve);
                 Frame.getFrames()[0].remove(this);
                 Frame.getFrames()[0].setVisible(true);
             } catch (Exception e) {
             }
-                Eleve eleve = new Eleve(jTextFieldEleve_nom.getText(), jTextFieldEleve_prenom.getText(), jTextFieldEleve_mail.getText());
+               
             Frame.getFrames()[0].add(new project_asi_1.Views.eleves_pop_up_create(eleve));
         }
 
